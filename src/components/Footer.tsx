62ec53e59dc788b879b2b13af64b33f24a78bc20
James Stomberg <james@vistrom.se>
2025-01-24 00:17:43 +0100

    Add footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,13 @@
+import '../assets/scss/Footer.scss';
+
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return (
+        <footer className="site-footer">
+            <div className="site-footer__inner">
+                <span>DT210G - Moment 2</span>
+                <span>Mittuniversitetet</span>
+                <span>James Stomberg</span>
+            </div>
+        </footer>
+    );
 }

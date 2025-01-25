0aa36eb3c8f3b1169e0958ac68cfe7f36bd727d3
James Stomberg <james@vistrom.se>
2025-01-25 23:54:36 +0100

    Prepare home page for blog posts and add intro text.

--- src/pages/Page.Home.tsx
+++ src/pages/Page.Home.tsx
@@ -1,7 +1,14 @@
 export default function Home({ title }: { title: string }) {
     return (
         <>
-            <h1>{title}</h1>
+            <section>
+                <h1>{title}</h1>
+                <p>Välkommen till den personliga bloggplattformen för James Stomberg. Ta gärna del av mina blogginlägg nedan.</p>
+            </section>
+
+            <section>
+                Blogginlägg
+            </section>
         </>
     );
 }
